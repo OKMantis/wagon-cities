8deff11e6b9bd093caca0493d6d522d4df106f16
Owen Moss <owenkmoss@gmail.com>
2018-06-24 12:51:43 +0200

    added image to city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 
 class City extends Component {
   render() {
+
+    const url = "https://kitt.lewagon.com/placeholder/cities/paris";
+
     return (
     <div className="active-city">
       <h3>{this.props.city.name}</h3>
       <p>{this.props.city.address}</p>
+      <img src={url} width="100%" />
     </div>
     );
   };

2d095bcd3188c33f63a542b2d889b83cdd4c2aa0
Owen Moss <owenkmoss@gmail.com>
2018-06-24 13:00:05 +0200

    imported bindActionCreators from redux, connect from redux and setCities from actions and added componentWillMount to trigger the action setCities and call the cities from the API which will then go to the citiesReducer to become a new Redux state, also added functions mapDispatchToProps to wire the setCities actions to the props of the cityList component and mapStateToProps to map the Redux state into the props of the component, finally added import connect to the these functions will be connected with cityList component

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions';
 
 import City from './city';
 
 class CityList extends Component {
+
+
+  componentWillMount() {
+    this.props.setCities();
+  }
+
   render() {
     return(
       <ul className="list-group cities">
@@ -13,4 +22,19 @@ class CityList extends Component {
 }
 
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+ return bindActionCreators(
+ { setCities: setCities },
+ dispatch
+ );
+}
+
+function mapStateToProps(state) {
+ return {
+ cities: state.cities
+ };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)
+(CityList);
